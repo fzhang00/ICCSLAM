4691e1271209a5c40531fc6aa5b2ad558a7db78b
Fan Zhang <fzhang00@gmail.com>
2013-06-08 23:43:26 -0400

    Edit chapter2 and chapter5
    Chapter 2, added citation for EKF consistency definition
    chapter 5: added text for introduction before showing convergence figure

---
+++ Thesis/auto/chapter2.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook "chapter2"
+ (lambda ()
+    (LaTeX-add-labels
+     "ch:Review"
+     "sec:sensor"
+     "sec:SensorOverview"
+     "sec:MonoBino"
+     "sec:OpticalSensorLimitation"
+     "sec:gps_and_imu"
+     "sec:SLAM"
+     "sec:SLAM_using_EKF"
+     "sec:SLAM_properties"
+     "sec:Linearization_error_and_consistency"
+     "sec:cam_centric")))
+
--- Thesis/auto/chapter5.el
+++ Thesis/auto/chapter5.el
@@ -1,8 +1,7 @@
 (TeX-add-style-hook "chapter5"
  (lambda ()
     (LaTeX-add-labels
-     "chapter:5"
-     "section:_Toc332876144"
+     "ch:FlightResult"
      "figure:_Ref349487278"
      "section:_Toc332876148")))
 
